3226bc2fde884d5903a481a3d30c19d0c2dab33c
liaoxuezhi <liaoxuezhi@baidu.com>
2014-05-13 15:06:45 +0800

    hide java dock

--- src/main/java/com/baidu/fis/server/launch/Main.java
+++ src/main/java/com/baidu/fis/server/launch/Main.java
@@ -20,6 +20,8 @@ public class Main {
     public static String WEBAPP_DIR = "./web";
 
     public static void main(String[] args) throws Exception {
+        System.setProperty("java.awt.headless", "true");
+
         Options options = new Options();
 
         options.addOption("h", "help", false, "output usage information" );
